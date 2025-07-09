ac25e7317b7ec72368c1bf31f9c870f1a0c5976d
Raül Box <rbv.bove@gmail.com>
2025-07-10 00:22:50 +0200

    index.txt files deleted

--- get_libs.sh
+++ get_libs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Descarga librerías necesarias en FONTS/lib
 
-mkdir -p FONTS/lib
-cd FONTS/lib || exit
+mkdir -p SRC/lib
+cd SRC/lib || exit
 
 echo "Downloading libraries..."
 
 # Gson
 curl -LO https://repo1.maven.org/maven2/com/google/code/gson/gson/2.9.0/gson-2.9.0.jar
 
-echo "✅ Libraries downloaded to FONTS/lib"
+echo "✅ Libraries downloaded to SRC/lib"
